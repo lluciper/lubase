2442f6ccfcb84db33e4273867acbbf2bc574fcb9
Luciper <hoangphuong@Luciper-2.local>
2022-10-11 23:53:21 +0700

    config fcm

---
+++ ios/lubasenoti/NotificationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NotificationViewController.swift
+//  lubasenoti
+//
+//  Created by Luciper on 11/10/2022.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
