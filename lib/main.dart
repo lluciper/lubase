bfe98eae19069d453684f57b9a003b9cfef7e1f7
nero <phuonghoang9910@gmail.com>
2023-02-21 14:46:03 +0700

    done fcm

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:name/router/app_router.dart';
 
@@ -39,6 +40,92 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key});
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+//   // Create a CollectionReference called users that references the firestore collection
+//   CollectionReference users = FirebaseFirestore.instance.collection('users');
+//   final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance
+//       .collection('users')
+//       .snapshots(includeMetadataChanges: true);
+
+//   Future<void> addUser({
+//     String fullName = '',
+//     String company = '',
+//     String age = '',
+//   }) {
+//     // Call the user's CollectionReference to add a new user
+//     return users
+//         .add({
+//           'fullName': fullName, // John Doe
+//           'company': company, // Stokes and Sons
+//           'age': age // 42
+//         })
+//         .then((value) => print("User Added"))
+//         .catchError((error) => print("Failed to add user: $error"));
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Flutter Demo Home Page'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             ElevatedButton(
+//               onPressed: () {
+//                 addUser(
+//                   fullName: 'John Doe',
+//                   company: 'Stokes and Sons',
+//                   age: '42',
+//                 );
+//                 // Navigator.pushNamed(context, 'customScreen');
+//               },
+//               child: const Text('Add User'),
+//             ),
+//             StreamBuilder<QuerySnapshot>(
+//               stream: _usersStream,
+//               builder: (BuildContext context,
+//                   AsyncSnapshot<QuerySnapshot> snapshot) {
+//                 if (snapshot.hasError) {
+//                   return Text('Something went wrong');
+//                 }
+
+//                 if (snapshot.connectionState == ConnectionState.waiting) {
+//                   return Text("Loading");
+//                 }
+
+//                 return ListView(
+//                   shrinkWrap: true,
+//                   children:
+//                       snapshot.data!.docs.map((DocumentSnapshot document) {
+//                     Map<String, dynamic> data =
+//                         document.data()! as Map<String, dynamic>;
+//                     return ListTile(
+//                       title: Text(data['fullName']),
+//                       subtitle: Text(data['company']),
+//                     );
+//                   }).toList(),
+//                 );
+//               },
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
@@ -47,28 +134,28 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  late final FirebaseMessaging _messaging;
 
-  // Create a CollectionReference called users that references the firestore collection
-  CollectionReference users = FirebaseFirestore.instance.collection('users');
-  final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance
-      .collection('users')
-      .snapshots(includeMetadataChanges: true);
+  void registerNotification() async {
+    // 1. Initialize the Firebase app
+    // await Firebase.initializeApp();
 
-  Future<void> addUser({
-    String fullName = '',
-    String company = '',
-    String age = '',
-  }) {
-    // Call the user's CollectionReference to add a new user
-    return users
-        .add({
-          'fullName': fullName, // John Doe
-          'company': company, // Stokes and Sons
-          'age': age // 42
-        })
-        .then((value) => print("User Added"))
-        .catchError((error) => print("Failed to add user: $error"));
+    // 2. Instantiate Firebase Messaging
+    _messaging = FirebaseMessaging.instance;
+
+    // 3. On iOS, this helps to take the user permissions
+    NotificationSettings settings = await _messaging.requestPermission(
+      alert: true,
+      badge: true,
+      provisional: false,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('User granted permission');
+    } else {
+      print('User declined or has not accepted permission');
+    }
   }
 
   @override
@@ -77,95 +164,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text('Flutter Demo Home Page'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                // addUser(
-                //   fullName: 'John Doe',
-                //   company: 'Stokes and Sons',
-                //   age: '42',
-                // );
-                Navigator.pushNamed(context, 'customScreen');
-              },
-              child: const Text('Add User'),
-            ),
-            StreamBuilder<QuerySnapshot>(
-              stream: _usersStream,
-              builder: (BuildContext context,
-                  AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (snapshot.hasError) {
-                  return Text('Something went wrong');
-                }
-
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Text("Loading");
-                }
-
-                return ListView(
-                  shrinkWrap: true,
-                  children:
-                      snapshot.data!.docs.map((DocumentSnapshot document) {
-                    Map<String, dynamic> data =
-                        document.data()! as Map<String, dynamic>;
-                    return ListTile(
-                      title: Text(data['fullName']),
-                      subtitle: Text(data['company']),
-                    );
-                  }).toList(),
-                );
-              },
-            )
-          ],
-        ),
-      ),
+      body: Container(),
     );
   }
 }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   late final FirebaseMessaging _messaging;
-
-//   void registerNotification() async {
-//     // 1. Initialize the Firebase app
-//     // await Firebase.initializeApp();
-
-//     // 2. Instantiate Firebase Messaging
-//     _messaging = FirebaseMessaging.instance;
-
-//     // 3. On iOS, this helps to take the user permissions
-//     NotificationSettings settings = await _messaging.requestPermission(
-//       alert: true,
-//       badge: true,
-//       provisional: false,
-//       sound: true,
-//     );
-
-//     if (settings.authorizationStatus == AuthorizationStatus.authorized) {
-//       print('User granted permission');
-//     } else {
-//       print('User declined or has not accepted permission');
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.title),
-//       ),
-//       body: Container(),
-//     );
-//   }
-// }
